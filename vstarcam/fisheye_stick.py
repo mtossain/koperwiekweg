f17c6102c5bd0a7b885d8f8a573c7a6c5f05ad93
Michel Tossaint <mmmtossaint@gmail.com>
2015-09-19 13:03:14 +0200

    Update fisheye_stick.py

--- vstarcam/fisheye_stick.py
+++ vstarcam/fisheye_stick.py
@@ -5,20 +5,24 @@ import ftplib
 import os
 import requests
 
-UpdateRate = 45 # In seconds
+dirstick = "/dev/test/"
+UpdateRate = 90 # In seconds
 
 while True:
     
     # Make the snapshot
-    os.system("ffmpeg -rtsp_transport tcp -y -i rtsp://admin:123456@192.168.178.254:554/mpeg4 -vcodec copy -t 3 snapshot.mp4")
-    os.system("ffmpeg -ss 00:00:02 -t 1 -y -i snapshot.mp4 -f mjpeg snapshot.jpeg")
     now = time.strftime("%Y-%m-%d %H:%M:%S")
+    snapshot = dirstick+"snapshot"+time.strftime("_%Y-%m-%d_%H:%M:%S")
+    os.system("ffmpeg -rtsp_transport tcp -y -i rtsp://admin:123456@192.168.178.254:554/mpeg4 -vcodec copy -t 3 snapshot"+now2+".mp4")
+    os.system("ffmpeg -ss 00:00:02 -t 1 -y -i "+snapshot+".mp4 -f mjpeg "+snapshot+".jpeg")
+    os.system("rm -f "+snapshot+".mp4")
+
     print(now + ' Snapshot taken [OK]')
 	
     #Convert to proper size and apply circle mask
-    os.system("convert -size 1280x960 xc:none -fill snapshot.jpeg -draw 'circle 640,480,1,480' domeflat.png")
-    os.system("convert domeflat.png -flop -resize 1024x1024 domeflatter.png")
-    os.system("convert -size 1024x1024 xc:none domeflatter.png -geometry +0+128 -composite dome.png")
+    os.system("convert -size 1280x960 xc:none -fill "+snapshot+".jpeg -draw 'circle 640,480,1,480' "+dirstick+"domeflat.png")
+    os.system("convert "+dirstick+"domeflat.png -flop -resize 1024x1024 "+dirstick+"domeflatter.png")
+    os.system("convert -size 1024x1024 xc:none "+dirstick+"domeflatter.png -geometry +0+128 -composite "+dirstick+"dome.png")
     now = time.strftime("%Y-%m-%d %H:%M:%S")
     print(now + ' Generated panorama [OK]')
 
@@ -26,7 +30,7 @@ while True:
     try:
         session = ftplib.FTP('server2.bhosted.nl','hjvveluw',Password)
         session.cwd('www/www.koperwiekweg.nl')
-        FileName = 'dome.png'
+        FileName = dirstick+"dome.png"
         file = open(FileName,'rb') # file to send
         session.storbinary('STOR '+FileName, file) # send the file
         file.close() # close file and FTP
