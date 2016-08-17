bd59fb15bf3e3639169870c8cca9f1e54be8fff2
Michel Tossaint <mmmtossaint@gmail.com>
2016-08-17 16:49:55 +0200

    Create overlay.py

---
+++ vstarcam/overlay.py
@@ -0,0 +1,99 @@
+import socket
+import time
+import ftplib
+import os
+import requests
+
+avail = 0 # which camera(s) is/are available 
+# 0  -both Fisheye
+# 1  -Fisheye1 only
+# 2  -Fisheye2 only
+RtspFisheye1 = 'rtsp://admin:admin@192.168.178.168:554/0/'
+RtspFisheye2 = 'rtsp://admin:123456@192.168.178.254:554/mpeg4'
+dirstick = "/media/DRAAKJE/" # where to put the snapshot pictures for archive
+UpdateRate = 45 # in s how often to make the snapshots
+Password = open('/home/pi/AuthBhostedFTP.txt','r').read().split('\n')[0] # password bhosted not stored in this script
+
+while True:
+    
+	if (avail==0 or avail=1):
+	
+        # Make the snapshot
+        print('1 Make the first snapshot\n\n')
+        now = time.strftime("%Y-%m-%d %H:%M:%S")
+        snapshot = dirstick+"snapshot"+time.strftime("_%Y%m%d_%H%M%S")+".jpeg"
+        os.system("avconv -r 6  -rtsp_transport tcp -y -i "+RtspFisheye1+" -f mp4 -an -vcodec copy -t 1 movie.mp4")
+        os.system("avconv -ss 00:00:00.5 -t 1 -y -i movie.mp4 -f mjpeg "+snapshot
+        os.system("rm -f movie.mp4")
+        print(now + ' Snapshot taken [OK]')
+		
+		#Convert to proper size and apply circle mask
+        print('2 Convert the snapshot\n\n')
+        os.system("convert "+snapshot+" -crop 2070x1920+285+0 "+dirstick+"dome1.png")
+        os.system("convert 2070x1920 xc:none -fill "+dirstick+"dome1.png -draw 'circle 1035,1010,2065,1010' "+dirstick+"dome2.png")
+        os.system("convert "+dirstick+"dome2.png -resize 1024x1024 -flip "+dirstick+"dome3.png")
+        os.system("convert -size 1024x1024 xc:none "+dirstick+"dome3.png -geometry +0+60 -composite "+dirstick+"dome4.png")
+        now = time.strftime("%Y-%m-%d %H:%M:%S")
+
+        # Move snapshot to directory
+        dailydir = dirstick+time.strftime("%Y%m%d")
+        os.system("mkdir "+dailydir)
+        os.system("mv "+snapshot+" "+dailydir)
+
+	if (avail==0 or avail=2):
+	
+		# Make the second snapshot
+        print('3 Make the second snapshot\n\n')
+        now = time.strftime("%Y-%m-%d %H:%M:%S")
+        snapshot = dirstick+"snapshot"+time.strftime("_%Y%m%d_%H%M%S")+".jpeg"
+        os.system("avconv -r 6  -rtsp_transport tcp -y -i "+RtspFisheye2+" -f mp4 -an -vcodec copy -t 1 movie.mp4")
+        os.system("avconv -ss 00:00:00.5 -t 1 -y -i movie.mp4 -f mjpeg "+snapshot
+        os.system("rm -f movie.mp4")
+        print(now + ' Snapshot taken [OK]')
+		
+		#Convert to proper size and apply circle mask
+        print('4 Convert the second snapshot\n\n')
+		os.system("del back*.png")
+         os.system("convert -size 1280x960 xc:none -fill "+snapshot+" -draw 'circle 640,480,1,480' "+dirstick+"back1.png")
+        os.system("convert "+dirstick+"back1.png -resize 1024x1024 -flip "+dirstick+"back2.png")
+        os.system("convert -size 1024x1024 xc:none "+dirstick+"back2.png -geometry +0+128 -composite "+dirstick+"back3.png")
+        os.system("convert -size 1024x1024 xc:none "+dirstick+"back3.png -rotate "-90" -composite "+dirstick+"back4.png")
+        now = time.strftime("%Y-%m-%d %H:%M:%S")
+
+ 	 	# Move second snapshot to directory
+        os.system("mv "+snapshot+" "+dailydir)
+
+    if (avail==0):
+        # Finally merge them together
+		print('5 Merge the pictures together\n\n')
+		os.system("composite -gravity center "+dirstick+"dome4.png "+dirstick+"back4.png "+dirstick+"dome.png")
+	if (avail==1)
+	    os.system("mv "+dirstick+"dome4.png "+dirstick+"dome.png")
+	if (avail==2)
+	    os.system("mv "+dirstick+"back4.png "+dirstick+"dome.png")
+
+    #Upload to FTP server
+    print('6 Move fisheye to FTP server')
+    try:
+        time.sleep(2)
+        FileName = dirstick+"dome.png"
+        file = open(FileName,'rb') # file to send
+        session = ftplib.FTP('server2.bhosted.nl','hjvveluw',Password,timeout=30)
+        session.cwd('www/www.koperwiekweg.nl')
+        session.storbinary('STOR dome.png', file) # send the file
+        file.close() # close file and FTP
+        session.quit()
+        print(now +' Uploaded fisheye '+FileName+' to FTP [OK]')
+    except (socket.error, ftplib.all_errors) as e:
+        print e
+        now = time.strftime("%Y-%m-%d %H:%M:%S")
+        print(now + ' Could not upload fisheye '+FileName+' to FTP [NOK]')
+
+    # Copy on the server, to avoid partly images on site when FTP ongoing
+    try:
+        r = requests.get('http://www.koperwiekweg.nl/copy_dome.php',timeout=15)
+    except (requests.ConnectionError, requests.Timeout, socket.timeout) as e:
+        print e
+        print(now +' Could not copy dome.png on server [NOK]')
+    
+    time.sleep(UpdateRate)
