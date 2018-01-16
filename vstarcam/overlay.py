4c7aabf32f620b4698a6034c059be7d08985343d
System Administrator <root@dhcp-10-176-36-152.estec.esa.int>
2018-01-16 14:45:46 +0100

    idjasdf;kadsjfl;j#

--- vstarcam/overlay.py
+++ vstarcam/overlay.py
@@ -31,7 +31,8 @@ avail = 0 # which camera(s) is/are available
 # 2  -Fisheye2 only
 RtspFisheye1 = 'rtsp://admin:admin@192.168.178.168:554/0/'
 RtspFisheye2 = 'rtsp://admin:123456@192.168.178.254:554/mpeg4'
-dirstick = "/media/pi/DRAAKJE/" # where to put the snapshot pictures for archive
+dirstick = "/ramtmp/" # where to put the snapshot pictures for archive
+#dirstick = "/media/pi/DRAAKJE/" # where to put the snapshot pictures for archive
 #dirstick = "/home/pi/" # where to put the snapshot pictures for archive
 UpdateRate = 90 # in s how often to make the snapshots
 Password = open('/home/pi/AuthBhostedFTP.txt','r').read().split('\n')[0] # password bhosted not stored in this script
@@ -40,7 +41,7 @@ while True:
     
     if (avail==0 or avail==1):
         
-        print('0 Set the camera in the right mode\n\n')
+        print('*** 0 - Set the camera in the right mode ***\n\n')
         try:
             r = requests.get('http://192.168.178.168/vb.htm?title=IP-Camera&videocodec=0&localdisplay=1&mirctrl=0&videocodeccombo=4&setvideoencbitratelevel=5&iframeinterval=30&codelevel=0', auth=HTTPBasicAuth('admin', 'admin'),timeout=15)
         except (requests.ConnectionError, requests.Timeout, socket.timeout) as e:
@@ -50,7 +51,7 @@ while True:
         #    print(response.read())
         time.sleep(3)
 		
-        print('1 Make the first snapshot\n\n')
+        print('*** 1 - Make the first snapshot ***\n\n')
         now = time.strftime("%Y-%m-%d %H:%M:%S")
         snapshot = dirstick+"snapshot"+time.strftime("_%Y%m%d_%H%M%S")+".jpeg"
         RunCmd(["./avconvv1.sh", "test"], 20).Run()
@@ -60,21 +61,16 @@ while True:
         os.system("rm -f movie.mp4")
         print(now + ' Snapshot taken [OK]')
 		
-        print('2 Convert the snapshot\n\n')
+        print('*** 2 - Convert the snapshot\n\n')
         os.system("convert "+snapshot+" -crop 2070x1920+285+0 "+dirstick+"dome1.png")
         os.system("convert -size 2070x1920 xc:none -fill "+dirstick+"dome1.png -draw 'circle 1035,1010,2065,1010' "+dirstick+"dome2.png")
         os.system("convert "+dirstick+"dome2.png -resize 1024x1024 -flip "+dirstick+"dome3.png")
         os.system("convert -size 1024x1024 xc:none "+dirstick+"dome3.png -geometry +0+60 -composite "+dirstick+"dome4.png")
         now = time.strftime("%Y-%m-%d %H:%M:%S")
 
-        print('3 Move first fisheye to external drive\n\n')
-        dailydir = dirstick+time.strftime("%Y%m%d")
-        os.system("mkdir "+dailydir)
-        os.system("mv "+snapshot+" "+dailydir)
-
     if (avail==0 or avail==2):
 	
-        print('4 Make the second snapshot\n\n')
+        print('*** 3 - Make the second snapshot ***\n\n')
         now = time.strftime("%Y-%m-%d %H:%M:%S")
         snapshot = dirstick+"snapshot"+time.strftime("_%Y%m%d_%H%M%S")+".jpeg"
         RunCmd(["./avconvv2.sh", "test"], 20).Run()
@@ -84,7 +80,7 @@ while True:
         os.system("rm -f movie.mp4")
         print(now + ' Snapshot taken [OK]')
 		
-        print('5 Convert the second snapshot\n\n')
+        print('*** 4 - Convert the second snapshot ***\n\n')
         os.system("convert -size 1280x960 xc:none -fill "+snapshot+" -draw 'circle 640,480,1,480' "+dirstick+"back1.png")
         os.system("convert "+dirstick+"back1.png -resize 1024x1024 "+dirstick+"back2.png")
         os.system("convert -size 1024x1024 xc:none "+dirstick+"back2.png -geometry +0+128 -composite "+dirstick+"back3.png")
@@ -92,10 +88,7 @@ while True:
         #os.system("convert "+dirstick+"back4.png +level-colors navy,lemonchiffon "+dirstick+"back5.png") # blueish filter...
         now = time.strftime("%Y-%m-%d %H:%M:%S")
 
-        print('6 Move first fisheye to external drive\n\n')
-        os.system("mv "+snapshot+" "+dailydir)
-
-    print('7 Merge the pictures together\n\n')
+    print('*** 5 - Merge the pictures together\n\n')
     if (avail==0):
         os.system("composite -gravity center "+dirstick+"dome4.png "+dirstick+"back4.png "+dirstick+"dome.png")
     if (avail==1):
@@ -106,7 +99,7 @@ while True:
     if (avail==2):
         os.system("mv "+dirstick+"back4.png "+dirstick+"dome.png")
 
-    print('8 Move fisheye to FTP server')
+    print('*** 6 - Move fisheye to FTP server')
     try:
         time.sleep(2)
         FileName = dirstick+"dome.png"
@@ -122,11 +115,14 @@ while True:
         now = time.strftime("%Y-%m-%d %H:%M:%S")
         print(now + ' Could not upload fisheye '+FileName+' to FTP [NOK]')
 
-    # Copy on the server, to avoid partly images on site when FTP ongoing
+    print('*** 7 - Copy image on server to avoid partly images on website when FTP ongoing')
     try:
         r = requests.get('http://www.koperwiekweg.nl/copy_dome.php',timeout=15)
     except (requests.ConnectionError, requests.Timeout, socket.timeout) as e:
         print (e)
         print(now +' Could not copy dome.png on server [NOK]')
     
+    os.system("rm -Rf "+dirstick+"*.jpeg")
+    os.system("rm -Rf "+dirstick+"*.png")
+
     time.sleep(UpdateRate)
