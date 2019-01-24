9a0b3e4aee1ba4f00beb7b2d741a82432a68ad75
Michel Tossaint <mmmtossaint@gmail.com>
2019-01-24 10:48:14 +0100

    easyprocess rain daemon

---
+++ master2018/rain_daemon3.py
@@ -0,0 +1,66 @@
+import shelve
+import datetime
+import os
+import time
+import json
+import subprocess
+import rpyc
+from easyprocess import Proc
+
+CRED   ='\033[91m'
+CGREEN ='\033[92m'
+CEND = '\033[0m'
+
+ram_drive            = '/ramtmp/'
+json_rain_sensor     = ram_drive+'rain.json'
+shelve_rain          = ram_drive+'rain'
+scale_factor         = 0.7
+#d = shelve.open(shelve_rain) # Save the data to file
+WeatherService = rpyc.connect("localhost", 18861)
+
+def nowStr():
+    return( datetime.datetime.now().strftime( '%Y-%m-%d %H:%M:%S'))
+
+ticks = 0
+temperature=0
+
+def getRainTicks():
+    global ticks
+    global temperature
+    os.system('rm -Rf '+json_rain_sensor)
+    time.sleep(0.5)
+    cmd = "rtl_433 -R 37 -E -F json:"+json_rain_sensor
+    try:
+        stdout=Proc(cmd).call(timeout=60).stdout
+        time.sleep(0.5)
+        with open(json_rain_sensor) as f:
+            data = json.loads(f.readline())
+        if "temperature_C" in data:
+            temperature = float(data["temperature_C"])
+        if "rain" in data:
+            ticks = int(data["rain"])
+        print(CGREEN+'[OK] ' + nowStr() + ' T:'+str(temperature)+' [degC] R:'+str(ticks)+' [ticks]'+CEND)
+    except:
+        print(CRED+'[NOK] rtl_433 timed out'+CEND)
+    return ticks, temperature
+
+
+first_tick,temperature = getRainTicks() # get the first tick
+print(CGREEN+'[OK] First tick: '+str(first_tick)+' [ticks]'+CEND)
+last_hour = datetime.datetime.now().hour
+
+# Main loop
+while(1):
+    ticks, temperature = getRainTicks()
+    hour = datetime.datetime.now().hour
+    if (hour==0) and (hour!=last_hour):
+        first_tick = ticks
+        print(CGREEN+'[OK] First tick: '+str(first_tick)+' [ticks]'+CEND)
+    rain = (ticks-first_tick)*scale_factor
+    try:
+        WeatherService.root.update_sensor_rain(rain,0)
+        print(CGREEN+'[OK] ' + nowStr() + ' Rain today:'+str(rain)+' [mm]'+CEND)
+    except:
+        print(CRED+'[NOK] Could not update weather service...'+CEND)
+    time.sleep(1)
+    last_hour = hour
