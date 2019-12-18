be3673a7df8285c01fc333811a49791a45893fe7
Michel Tossaint <mmmtossaint@gmail.com>
2019-12-18 14:58:39 +0100

    pushed some changes on 2018

--- slave2018/scrape_slave.py
+++ slave2018/scrape_slave.py
@@ -3,9 +3,7 @@
 # 2018 - M.Tossaint
 ###############################################################################
 import os
-os.system('modprobe i2c_bcm2835 baudrate=400000')
 import time
-time.sleep(3) # Wait for baudrate to change
 import shelve
 import ftplib
 import datetime
@@ -15,11 +13,9 @@ CRED = '\033[91m'
 CGREEN = '\033[92m'
 CEND = '\033[0m'
 
-flag_upload_to_master = False
-flag_camera           = False
+flag_upload_to_master = True
+flag_camera           = True
 flag_mcp9808          = False
-flag_sht3x            = True
-flag_ams811           = True
 ftp_server            = open("/home/pi/MasterIP.txt",'r').read().split('\n')[0]
 ftp_username          = 'pi'
 ftp_password          = open("/home/pi/AuthMasterPi.txt",'r').read().split('\n')[0]
@@ -29,7 +25,7 @@ def nowStr():
     return( datetime.datetime.now().strftime( '%Y-%m-%d %H:%M:%S'))
 
 try:
-    WeatherService2 = rpyc.connect(ftp_server, 18861)
+    WeatherService = rpyc.connect(ftp_server, 18861)
     print(CGREEN+'[OK] Connected to the WeatherServer')
 except:
     print(CRED+'[NOK] Not connected to the WeatherServer')
@@ -47,17 +43,6 @@ try:
     print(CGREEN+'[OK]  '+nowStr()+' BME280 T: '+str(temperature)+' [degC] P: '+str(pressure) + ' [mBar] H: '+str(humidity)+' [%]')
 except:
     print(CRED+'[NOK] '+nowStr()+' Could not find the data from the BME280 pressure and humidity'+CEND)
-
-
-if flag_sht3x:
-    from read_sht3x import *
-    try:
-        humidity, temperature_sht3x = get_sht3x_data()
-        temperature_sht3x = round(temperature_sht3x,1)
-        humidity = round(humidity,1)
-        print(CGREEN+'[OK]  '+nowStr()+' SHT3X H:'+str(humidity)+' [%] T: '+str(temperature_sht3x)+' [degC]')
-    except:
-        print(CRED+'[NOK] '+nowStr()+' Could not find the data from the SHT3X data')
 
 
 if flag_mcp9808:
@@ -85,33 +70,17 @@ except:
 
 if flag_camera:
     try:
-        os.system('raspistill -h 1054 -w 1054 -o '+local_path+'cam.jpg') # Take the camera image
-        os.system('raspistill -h 1944 -w 1944 -o '+local_path+'cam_hd.jpg') # Take the camera image
+        os.system('sudo raspistill -h 1054 -w 1054 -o '+local_path+'cam.jpg') # Take the camera image
+        os.system('sudo raspistill -h 1944 -w 1944 -o '+local_path+'cam_hd.jpg') # Take the camera image
         print('[OK]  '+nowStr()+' Got the camera image: '+local_path+'cam.jpg')
     except:
         print(CRED+'[NOK] '+nowStr()+' Could not take the camera image'+CEND)
 
-
-# Assume temperature is from BME280 or MCP9808
-os.system('modprobe i2c_bcm2835 baudrate=10000')
-time.sleep(3)
-co2=0
-tvoc=0
-if flag_ams811:
-    from read_ams811 import *
-    try:
-        co2,tvoc = get_ams811_data(temperature)
-        print(CGREEN+'[OK]  '+nowStr()+' AMS811 eCO2: '+str(co2)+' [ppm] TVOC: '+str(tvoc)+' [ppm]')
-    except:
-        print(CRED+'[NOK] '+nowStr()+' Could not find AMS811 CO2 data'+CEND)
-
-
 try:
-    #WeatherService2.root.update_sensor_2018(temperature,pressure,humidity,uv_index,light_intensity,nowStr())
-    WeatherService2.root.update_sensor_2019(temperature,pressure,humidity,uv_index,light_intensity,co2,tvoc,nowStr())
-    print(CGREEN+'[OK] Uploaded data to Weather server'+CEND)
+    WeatherService.root.update_sensor_2018(temperature,pressure,humidity,uv_index,light_intensity,nowStr())
+    print(CGREEN+'[OK] Uploaded data to WeatherServer'+CEND)
 except:
-    print(CRED+'[NOK] Could not update Weather server...'+CEND)
+    print(CRED+'[NOK] Could not update WeatherServer...'+CEND)
 
 if flag_upload_to_master:
     try:
@@ -123,6 +92,6 @@ if flag_upload_to_master:
             fh = open(local_path+"cam_hd.jpg", 'rb')
             ftp_connection.storbinary('STOR /ramtmp/cam_hd.jpg', fh)
             fh.close()
-        print('[OK]  '+nowStr()+' Uploaded skyview to the ftp master: '+ftp_server)
+        print(CGREEN+'[OK]  '+nowStr()+' Uploaded skyview to the ftp master: '+ftp_server)
     except:
         print(CRED+'[NOK] '+nowStr()+' Could not upload skyview to the ftp master.'+CEND)
